c32223a3b2fc9312e8a9f6b089a19eb48956c5f9
eliawebdesign <56521069+eliawebdesign@users.noreply.github.com>
2020-04-30 16:07:56 +0200

    Add files via upload
    Basic files

---
+++ includes/class-elia-webdesign-plugin-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://elia-webdesign.nl
+ * @since      1.0.0
+ *
+ * @package    Elia_Webdesign_Plugin
+ * @subpackage Elia_Webdesign_Plugin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Elia_Webdesign_Plugin
+ * @subpackage Elia_Webdesign_Plugin/includes
+ * @author     Sebastiaan Gouw <info@elia-webdesign.nl>
+ */
+class Elia_Webdesign_Plugin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-elia-webdesign-plugin-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://elia-webdesign.nl
+ * @since      1.0.0
+ *
+ * @package    Elia_Webdesign_Plugin
+ * @subpackage Elia_Webdesign_Plugin/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Elia_Webdesign_Plugin
+ * @subpackage Elia_Webdesign_Plugin/includes
+ * @author     Sebastiaan Gouw <info@elia-webdesign.nl>
+ */
+class Elia_Webdesign_Plugin_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
